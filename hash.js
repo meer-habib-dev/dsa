707aaabe0f8147afd68edf62275d58e8ed95c27c
meer-habib-dev <contact.freelancermir@gmail.com>
2026-08-08 22:55:10 +0600

    count

--- hash.js
+++ hash.js
@@ -1,111 +1,129 @@
-// // // // // // find the unique element;
+// // // // // // // find the unique element;
 
-// // // // // const arr = [10, 2, 10, 2, 3, 4, 4];
+// // // // // // const arr = [10, 2, 10, 2, 3, 4, 4];
 
-// // // // // const hash = new Set();
+// // // // // // const hash = new Set();
+
+// // // // // // for (let item of arr) {
+// // // // // //   if (hash.has(item)) {
+// // // // // //     continue;
+// // // // // //   } else {
+// // // // // //     hash.add(item);
+// // // // // //   }
+// // // // // // }
+
+// // // // // // console.log(hash);
+
+// // // // // // pangram;
+
+// // // // // // const str = "thequickbrownfoxjumpsoverthelazydog";
+
+// // // // // // // const has = new Set(str);
+// // // // // // let has = new Set();
+
+// // // // // // for (let i = 0; i < str.length; i++) {
+// // // // // //   has.add(str.charAt(i));
+// // // // // // }
+
+// // // // // // console.log("has", has.size);
+
+// // // // // // check the frequency of the element in the array;
+
+// // // // // const arr = [2, 2, 3, 4, 4, 4, 10, 8, 9, 10];
+
+// // // // // const hash = new Map();
 
 // // // // // for (let item of arr) {
 // // // // //   if (hash.has(item)) {
-// // // // //     continue;
+// // // // //     hash.set(item, hash.get(item) + 1);
 // // // // //   } else {
-// // // // //     hash.add(item);
+// // // // //     hash.set(item, 1);
 // // // // //   }
 // // // // // }
 
 // // // // // console.log(hash);
 
-// // // // // pangram;
+// // // // let names = ["Mary", "John", "Emma"],
+// // // //   heights = [180, 165, 170];
+// // // // let hash = new Map();
 
-// // // // // const str = "thequickbrownfoxjumpsoverthelazydog";
+// // // // let i = 0;
+// // // // let j = heights.length - 1;
 
-// // // // // // const has = new Set(str);
-// // // // // let has = new Set();
+// // // // for (let i = 0; i < heights.length; i++) {
+// // // //   hash.set(heights[i], names[i]);
+// // // // }
+// // // // heights.sort((a, b) => b - a);
 
-// // // // // for (let i = 0; i < str.length; i++) {
-// // // // //   has.add(str.charAt(i));
-// // // // // }
-
-// // // // // console.log("has", has.size);
-
-// // // // // check the frequency of the element in the array;
-
-// // // // const arr = [2, 2, 3, 4, 4, 4, 10, 8, 9, 10];
-
-// // // // const hash = new Map();
-
-// // // // for (let item of arr) {
-// // // //   if (hash.has(item)) {
-// // // //     hash.set(item, hash.get(item) + 1);
-// // // //   } else {
-// // // //     hash.set(item, 1);
-// // // //   }
+// // // // for (let i = 0; i < heights.length; i++) {
+// // // //   names[i] = hash.get(heights[i]);
 // // // // }
 
-// // // // console.log(hash);
+// // // // console.log(hash, heights, names);
 
-// // // let names = ["Mary", "John", "Emma"],
-// // //   heights = [180, 165, 170];
+// // // let nums = [3, 2, 4];
+// // // target = 6;
+
 // // // let hash = new Map();
 
-// // // let i = 0;
-// // // let j = heights.length - 1;
-
-// // // for (let i = 0; i < heights.length; i++) {
-// // //   hash.set(heights[i], names[i]);
-// // // }
-// // // heights.sort((a, b) => b - a);
-
-// // // for (let i = 0; i < heights.length; i++) {
-// // //   names[i] = hash.get(heights[i]);
+// // // for (let i = 0; i < nums.length; i++) {
+// // //   if (hash.has(target - nums[i])) {
+// // //     return [i, hash.get(target - nums[i])];
+// // //   } else {
+// // //     hash.set(nums[i], i);
+// // //   }
 // // // }
 
-// // // console.log(hash, heights, names);
+// // // console.log(" nums.indexOf(7);", indexes);
 
-// // let nums = [3, 2, 4];
-// // target = 6;
+// // let nums1 = [1, 2, 2, 1],
+// //   nums2 = [2, 2];
 
-// // let hash = new Map();
-
-// // for (let i = 0; i < nums.length; i++) {
-// //   if (hash.has(target - nums[i])) {
-// //     return [i, hash.get(target - nums[i])];
-// //   } else {
-// //     hash.set(nums[i], i);
+// // let ans = new Set([]);
+// // for (let i = 0; i < nums1.length; i++) {
+// //   for (let j = 0; j < nums2.length; j++) {
+// //     if (nums2[j] === nums1[i]) {
+// //       ans.add(nums1[i]);
+// //     } else {
+// //       continue;
+// //     }
 // //   }
 // // }
 
-// // console.log(" nums.indexOf(7);", indexes);
+// // console.log("ans", Array.from(ans));
 
 // let nums1 = [1, 2, 2, 1],
 //   nums2 = [2, 2];
+// let ans = [];
+// let set = new Set(nums1);
 
-// let ans = new Set([]);
-// for (let i = 0; i < nums1.length; i++) {
-//   for (let j = 0; j < nums2.length; j++) {
-//     if (nums2[j] === nums1[i]) {
-//       ans.add(nums1[i]);
-//     } else {
-//       continue;
-//     }
+// for (let i = 0; i < nums2.length; i++) {
+//   if (set.has(nums2[i]) && !ans.includes(nums2[i])) {
+//     ans.push(nums2[i]);
+//   } else {
+//     continue;
 //   }
 // }
 
-// console.log("ans", Array.from(ans));
+// console.log("set", ans);
 
-let nums1 = [1, 2, 2, 1],
-  nums2 = [2, 2];
-let ans = [];
-let set = new Set(nums1);
+// let nums = [1, 1, 1],
+//   k = 2;
 
-for (let i = 0; i < nums2.length; i++) {
-  if (set.has(nums2[i]) && !ans.includes(nums2[i])) {
-    ans.push(nums2[i]);
-  } else {
-    continue;
+let nums = [1, 2, 3];
+let k = 3;
+
+let sum = 0;
+let count = 0;
+let hash = new Map();
+hash.set(0, 1);
+
+for (let i = 0; i < nums.length; i++) {
+  sum += nums[i];
+
+  if (hash.has(sum - k)) {
+    count += hash.get(sum - k);
   }
+  hash.set(sum, (hash.get(sum) || 0) + 1);
 }
-
-console.log("set", ans);
-
-
-
+console.log("hash", hash, count);
