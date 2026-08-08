b30f6995eb5c6b77494ae6734b5aff3f459b2356
meer-habib-dev <contact.freelancermir@gmail.com>
2026-08-08 15:30:35 +0600

    pandrome

--- hash.js
+++ hash.js
@@ -1,15 +1,23 @@
-// find the unique element;
+// // find the unique element;
 
-const arr = [10, 2, 10, 2, 3, 4, 4];
+// const arr = [10, 2, 10, 2, 3, 4, 4];
 
-const hash = new Set();
+// const hash = new Set();
 
-for (let item of arr) {
-  if (hash.has(item)) {
-    hash.delete(item);
-  } else {
-    hash.add(item);
-  }
-}
+// for (let item of arr) {
+//   if (hash.has(item)) {
+//     continue;
+//   } else {
+//     hash.add(item);
+//   }
+// }
 
-console.log(hash);
+// console.log(hash);
+
+// pangram;
+
+const str = "thequickbrownfoxjumpsoverthelazydog";
+
+const has = new Set(str);
+
+console.log("has", has.size);
