2b2fe107cb1e87b26a8cffbf16d25161d40a181d
meer-habib-dev <contact.freelancermir@gmail.com>
2026-08-10 19:35:28 +0600

    longest conces with time

--- longest-conse.js
+++ longest-conse.js
@@ -1,16 +1,34 @@
-let arr = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
+// let arr = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
 
-let count = 1;
+// let count = 1;
+// let longest = 0;
+// arr.sort((a, b) => a - b);
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === arr[i - 1]) continue;
+//   else if (arr[i] === arr[i - 1] + 1) count++;
+//   else {
+//     count = 1;
+//   }
+//   console.log("l", longest, count);
+//   longest = Math.max(longest, count);
+// }
+// console.log("arr", arr, count, longest);
+let arr = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
+let set = new Set(arr);
+let count = 0;
 let longest = 0;
-arr.sort((a, b) => a - b);
 
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i] === arr[i - 1]) continue;
-  else if (arr[i] === arr[i - 1] + 1) count++;
-  else {
+  let value = arr[i];
+  if (set.has(value + count)) {
+    while (set.has(value + count)) {
+      count++;
+    }
+  } else {
     count = 1;
   }
-  console.log("l", longest, count);
   longest = Math.max(longest, count);
 }
-console.log("arr", arr, count, longest);
+
+console.log("arr", count, longest);
