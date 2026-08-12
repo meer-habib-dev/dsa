dfd19ef2f5e7dea11f08f9a1e24d29e86333be70
meer-habib-dev <contact.freelancermir@gmail.com>
2026-08-12 12:42:04 +0600

    maj ele hash

---
+++ hashing/169. Majority Element.js
@@ -0,0 +1,29 @@
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+let nums = [2, 2, 1, 1, 1, 2, 2];
+
+   let ele = new Map();
+   let majEle = 0;
+
+   for (let i = 0; i < nums.length; i++) {
+     let currentVal = nums[i];
+     let storedValue = ele.get(currentVal);
+     ele.set(currentVal, (storedValue || 0) + 1);
+
+     let tLength = nums.length / 2;
+     storedValue = ele.get(currentVal);
+     if (storedValue > tLength) {
+       majEle = currentVal;
+     }
+   }
+
+   return majEle;
+
+console.log("ele", ele, maj);
--- random/intersection.js
+++ random/intersection.js
@@ -1,13 +1,13 @@
 let nums1 = [4, 9, 5],
   nums2 = [9, 4, 9, 8, 4];
 
-let arr = [];
 let numsOne = new Set(nums1);
+let result = new Set();
 
 for (let i = 0; i < nums2.length; i++) {
-  if (numsOne.has(nums2[i]) && !arr.includes(nums2[i])) {
-    arr.push(nums2[i]);
+  if (numsOne.has(nums2[i])) {
+    result.add(nums2[i]);
   }
 }
 
-console.log("arr", arr);
+console.log("arr", [...result]);
