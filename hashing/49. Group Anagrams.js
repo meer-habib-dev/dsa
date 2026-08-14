1e49d5255a1493dab819d44cc91cfba822a0fa29
meer-habib-dev <contact.freelancermir@gmail.com>
2026-08-14 17:17:03 +0600

    49 fix

--- hashing/49. Group Anagrams.js
+++ hashing/49. Group Anagrams.js
@@ -3,11 +3,44 @@ let strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
 function groupAnagrams() {
   let strMap = new Map();
 
+  let result = [];
+
   for (let i = 0; i < strs.length; i++) {
-    strMap(strMap[i], ["bit"]);
+    let check = strs[i].split("").sort().join("");
+
+    if (strMap.has(check)) {
+      let val = strMap.get(check);
+      val.push(strs[i]);
+      strMap.set(check, val);
+      continue;
+    }
+
+    strMap.set(check, [strs[i]]);
   }
 
-  console.log(strMap);
+  for (let arr of strMap.values()) {
+    result.push(arr);
+  }
+  console.log(result);
 }
 
 console.log(groupAnagrams());
+
+//  if (i === 0) {
+//       result.push(strs[i]);
+//       continue;
+//     }
+//     let count = 0;
+//     for (let j = 0; j < strs[i].length; j++) {
+//       if (result[0].includes(strs[i][j])) {
+//         count++;
+//       } else {
+//         count = 0;
+//       }
+//       console.log("jc", j + 1, count);
+//       if (j === count) {
+//         count = 0;
+//         result.push(strs[i]);
+//       }
+//     }
+//     console.log();
