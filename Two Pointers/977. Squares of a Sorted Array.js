485d8e74f560f810b38e4cd74ffbdb8adf67151a
meer-habib-dev <contact.freelancermir@gmail.com>
2026-08-20 22:22:58 +0600

    977 wip

--- Two Pointers/977. Squares of a Sorted Array.js
+++ Two Pointers/977. Squares of a Sorted Array.js
@@ -1,8 +1,28 @@
-let nums = [-4, -1, 0, 3, 10];
+let nums = [-5, -3, -2, -1];
+
+let i = 0;
+let j = nums.length - 1;
 
 for (let i = 0; i < nums.length; i++) {
   let squired = nums[i] ** 2;
   nums[i] = squired;
 }
 
-console.log(nums.sort((a, b) => a - b));
+console.log(nums);
+while (i <= j) {
+  let a = nums[i];
+  let b = nums[j];
+
+  if (a > b) {
+    let temp = nums[j];
+    nums[j] = nums[i];
+    nums[i] = temp;
+    j--;
+  } else if (i + 1 === j) {
+    i++;
+  } else {
+    j--;
+  }
+}
+
+console.log(nums);
