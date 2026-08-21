d5820d38ec7cb25d0862ea41a973d28a7df2e00a
meer-habib-dev <contact.freelancermir@gmail.com>
2026-08-22 00:44:19 +0600

    end

--- js-prep/prototype.js
+++ js-prep/prototype.js
@@ -1,20 +1,33 @@
-const user = {
-  name: "meer",
+// const user = {
+//   name: "meer",
+// };
+
+// console.log(Object.hasOwn(user, "name"));
+// console.log(Object.hasOwn(user, "toString"));
+// console.log("toString" in user);
+// console.log(Object.prototype);
+
+// const child = Object.create(user);
+
+// child.name = "farju";
+
+// console.log("", user.name, child.name);
+// delete child.name;
+// console.log("", user.name, child.name);
+// const arr = [1, 2];
+// console.log(Object.getPrototypeOf(user) === Object.prototype);
+// console.log(Object.getPrototypeOf(arr) === Array.prototype);
+// console.log(user.__proto__ === Object.prototype);
+
+function User(name) {
+  this.name;
+}
+
+const meer = new User("meer");
+const habib = new User("habib");
+
+User.prototype.introduce = function () {
+  console.log("hello", this.name);
 };
 
-console.log(Object.hasOwn(user, "name"));
-console.log(Object.hasOwn(user, "toString"));
-console.log("toString" in user);
-console.log(Object.prototype);
-
-const child = Object.create(user);
-
-child.name = "farju";
-
-console.log("", user.name, child.name);
-delete child.name;
-console.log("", user.name, child.name);
-const arr = [1, 2];
-console.log(Object.getPrototypeOf(user) === Object.prototype);
-console.log(Object.getPrototypeOf(arr) === Array.prototype);
-console.log(user.__proto__ === Object.prototype);
+console.log(meer.prototype.introduce);
