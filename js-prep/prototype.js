600d25dfbe75b12b1fc25d67e013c7200185c78f
meer-habib-dev <contact.freelancermir@gmail.com>
2026-08-21 23:03:13 +0600

    obj type

--- js-prep/prototype.js
+++ js-prep/prototype.js
@@ -6,3 +6,11 @@ console.log(Object.hasOwn(user, "name"));
 console.log(Object.hasOwn(user, "toString"));
 console.log("toString" in user);
 console.log(Object.prototype);
+
+const child = Object.create(user);
+
+child.name = "farju";
+
+console.log("", user.name, child.name);
+delete child.name;
+console.log("", user.name, child.name);
