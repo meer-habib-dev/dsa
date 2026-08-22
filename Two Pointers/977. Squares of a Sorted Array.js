e55c94bfb34d373d3a16fae7f6e18cd84469eb67
meer-habib-dev <contact.freelancermir@gmail.com>
2026-08-22 23:24:07 +0600

    977 fixed

--- Two Pointers/977. Squares of a Sorted Array.js
+++ Two Pointers/977. Squares of a Sorted Array.js
@@ -1,28 +1,49 @@
-let nums = [-5, -3, -2, -1];
+// let nums = [-5, -3, -2, -1];
+
+// let i = 0;
+// let j = nums.length - 1;
+
+// for (let i = 0; i < nums.length; i++) {
+//   let squired = nums[i] ** 2;
+//   nums[i] = squired;
+// }
+
+// console.log(nums);
+// while (i <= j) {
+//   let a = nums[i];
+//   let b = nums[j];
+
+//   if (a > b) {
+//     let temp = nums[j];
+//     nums[j] = nums[i];
+//     nums[i] = temp;
+//     j--;
+//   } else if (i + 1 === j) {
+//     i++;
+//   } else {
+//     j--;
+//   }
+// }
+
+// console.log(nums);
+
+let nums = [-4, -1, 0, 3, 10];
+
+let result = Array();
 
 let i = 0;
 let j = nums.length - 1;
 
-for (let i = 0; i < nums.length; i++) {
-  let squired = nums[i] ** 2;
-  nums[i] = squired;
-}
-
-console.log(nums);
 while (i <= j) {
-  let a = nums[i];
-  let b = nums[j];
+  let a = nums[i] ** 2;
+  let b = nums[j] ** 2;
 
   if (a > b) {
-    let temp = nums[j];
-    nums[j] = nums[i];
-    nums[i] = temp;
-    j--;
-  } else if (i + 1 === j) {
+    result.push(a);
     i++;
   } else {
+    result.push(b);
     j--;
   }
 }
-
-console.log(nums);
+console.log("res", result.reverse());
