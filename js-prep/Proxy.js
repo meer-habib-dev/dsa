ff8a7d30905220bdc1371f3d393da932e2865ecc
meer-habib-dev <contact.freelancermir@gmail.com>
2026-08-24 11:38:55 +0600

    debounce

--- js-prep/Proxy.js
+++ js-prep/Proxy.js
@@ -10,3 +10,20 @@ const proxyhandle = new Proxy(user, {
 });
 
 console.log(proxyhandle.name);
+const user = {
+  name: "Meer",
+};
+
+const proxy = new Proxy(user, {
+  get(target, property, receiver) {
+    console.log(`Reading ${String(property)}`);
+
+    return Reflect.get(target, property, receiver);
+  },
+
+  set(target, property, value, receiver) {
+    console.log(`Updating ${String(property)}`);
+
+    return Reflect.set(target, property, value, receiver);
+  },
+});
